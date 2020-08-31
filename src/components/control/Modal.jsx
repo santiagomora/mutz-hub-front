7e9ac651cb9d28d44c727e6ca4dfbc028fc10c75
Luis <santiagomorad@gmail.com>
2020-08-30 22:44:42 -0300

    merging production ands master 1

---
+++ src/components/control/Modal.jsx
@@ -0,0 +1,27 @@
+import React, {
+    Component
+} from 'react';
+
+import ReactModal from 'react-modal';
+
+const customStyles = {
+    content : {
+        top     : '5%',
+        left    : '15%',
+        right   : 'auto',
+        bottom  : 'auto',
+        width   : '70%',
+        maxHeight: '90%'
+    }
+};
+
+export default function Modal(props) {
+    return(
+        <ReactModal
+            ariaHideApp={false}
+            isOpen={props.show}
+            style = {customStyles}>
+            {props.children}
+        </ReactModal>
+    );
+}

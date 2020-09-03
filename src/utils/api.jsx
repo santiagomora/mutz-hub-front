6c2103c7c4d92e3505d351ed2830a878e206b6b1
Luis <santiagomorad@gmail.com>
2020-09-03 00:35:31 -0300

    trying to fix

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = 'https://mutz-hub-back.herokuapp.com';//'http://127.0.0.1:8000';
 
-export const RESOURCE_URL = 'http://127.0.0.1:3000';
+export const RESOURCE_URL = 'https://mutz-hub.herokuapp.com';//'http://127.0.0.1:3000';
 
 const API_BASE = `${BASE_URL}/api`;
 

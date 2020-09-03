71997767dccb1f3095e666f3280d08bc3c9e5a6e
Luis <santiagomorad@gmail.com>
2020-09-03 01:03:15 -0300

    fixing some responsive issues

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://127.0.0.1:8000';//'https://mutz-hub-back.herokuapp.com';
+//export const BASE_URL = 'http://127.0.0.1:8000';
 
-export const RESOURCE_URL = 'http://127.0.0.1:3000';//'https://mutz-hub.herokuapp.com';//'http://127.0.0.1:3000';
+export const BASE_URL = 'https://mutz-hub-back.herokuapp.com';
+
+//export const RESOURCE_URL = 'http://127.0.0.1:3000';
+
+export const RESOURCE_URL = https://mutz-hub.herokuapp.com';//'http://127.0.0.1:3000';
 
 console.log("hila")
 
